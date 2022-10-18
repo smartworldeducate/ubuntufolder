d877430081cfa170739e67ee38d9561eb90480fd
Umar Younas <umar.younas@codeupscale.com>
2022-10-18 14:29:00 +0500

    Umar's initial commit

--- SRC/Components/FlatList/FlatList.js
+++ SRC/Components/FlatList/FlatList.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import {StyleSheet, FlatList} from 'react-native';
 
+const FlatListItem = ({
+  data,
+  renderItem,
+  horizontal,
+  numColumns,
+  keyExtractor,
+  ListEmptyComponent,
+  initialNumToRender,
+  ItemSeparatorComponent,
+}) => {
+  return (
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      horizontal={horizontal}
+      numColumns={numColumns}
+      keyExtractor={keyExtractor}
+      ListEmptyComponent={ListEmptyComponent}
+      initialNumToRender={initialNumToRender}
+      ItemSeparatorComponent={ItemSeparatorComponent}
+    />
+  );
+};
 
-const FlatListItem = ({ data, renderItem, horizontal, numColumns, keyExtractor, ListEmptyComponent, initialNumToRender, ItemSeparatorComponent }) => {
-
-    return (
-        <FlatList
-            data={data}
-            renderItem={renderItem}
-            horizontal={horizontal}
-            numColumns={numColumns}
-            keyExtractor={keyExtractor}
-            ListEmptyComponent={ListEmptyComponent}
-            initialNumToRender={initialNumToRender}
-            ItemSeparatorComponent={ItemSeparatorComponent}
-        />
-    );
-}
-
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 export default FlatListItem;
